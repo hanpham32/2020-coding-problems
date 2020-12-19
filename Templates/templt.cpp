250eff0e9d76a147f044452ed5e21fe8ef8607b1
hanpham32 <hanpham3230@gmail.com>
2020-12-18 16:36:56 -0800

    templt

---
+++ Templates/templt.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(){
+	
+}
+
+int main() {
+	fast_io;
+
+	int t;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
