965103155c1224c5793513455eb215ca33b643c7
hanpham32 <hanpham3230@gmail.com>
2020-12-19 13:02:37 -0800

    a

--- LeetCode/longestPalindromicSubstring.cpp
+++ LeetCode/longestPalindromicSubstring.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-	string longestPalindrome(string s) {
-		if (s.empty()) return "";
-		if (s.size() == 1) return s;
-		int min_start = 0, max_len = 1;
-		for (int i=0; i<s.size();){
-			if (s.size() - i <= max_len / 2) break;
-			int j=i, k=i;
-			while (k<s.size()-1 && s[k+1] == s[k]) ++k;
-			i = k+1;
-			while (k < s.size()-1 && j > 0 && s[k+1] == s[j-1]) {++k; --j;} // expand
-			int new_len = k - j + 1;
-			if (new_len > max_len) {min_start = j; max_len = new_len;}
-		}
-		return s.substr(min_start, max_len);
-	}
-}
+    string longestPalindrome(string s) {
+        int first = 0, last = 1;
+        for (int i=0; i<s.size(); ){
+            int j = i, k = i;
+            
+            // Skip duplicates in the middle
+            while (k+1 < s.size() && s[k+1] == s[k]) { k++;}
+            i= k+1;
+            
+            while (j-1 >= 0 && k + 1 < s.length() && s[j-1] == s[k+1]) {
+                j--;
+                k++;
+            }
+            
+            int currLen = k - j +1;
+            if(currLen > last) {
+                first = j;
+                last = currLen;
+            }
+        }
+        return s.substr(first, last);
+    }
+};
