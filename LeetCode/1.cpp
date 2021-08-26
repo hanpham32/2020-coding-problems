deec9eef9aaa5010f810aa5acbdaeb19154c4772
hanpham32 <yuniroiep@gmail.com>
2021-08-26 00:17:13 -0700

    Renamed files

---
+++ LeetCode/1.cpp
@@ -0,0 +1,11 @@
+using namespace std::vector;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j=1+i; i< nums.size(); i++) {
+
+            }
+        }
+    }
+};
